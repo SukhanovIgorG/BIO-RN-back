5278253c682c8770a02d9ad877a7a42454bf76e3
SukhanovIgorG <sukhanovgarik@yandex.ru>
2025-08-03 20:00:58 +0300

    feat: add description dtos

--- src/auth/dto/login-auth.dto.ts
+++ src/auth/dto/login-auth.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class LoginAuthDto {
+  @ApiProperty({ example: '8m5yR@example.com' })
   email: string;
+
+  @ApiProperty({ example: '12345678' })
   password: string;
 }
--- src/auth/dto/register-auth.dto.ts
+++ src/auth/dto/register-auth.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class RegisterAuthDto {
+  @ApiProperty({ example: 'John Doe' })
   username: string;
+
+  @ApiProperty({ example: '8m5yR@example.com' })
   email: string;
+
+  @ApiProperty({ example: '12345678' })
   password: string;
 }
